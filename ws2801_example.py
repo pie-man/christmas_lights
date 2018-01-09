64b280b402035b1465b89c2f6a77093d5bb5f4f2
Simon <pie-man@users.noreply.github.com>
2018-01-09 19:31:18 +0000

    Update ws2801_example.py

--- ws2801_example.py
+++ ws2801_example.py
@@ -183,16 +183,14 @@ if __name__ == "__main__":
     brightness_decrease(pixels)
     
     appear_from_back(pixels)
-    
+
     #for i in range(3):
     #    blink_color(pixels, blink_times = 1, color=(255, 0, 0))
     #    blink_color(pixels, blink_times = 1, color=(0, 255, 0))
     #    blink_color(pixels, blink_times = 1, color=(0, 0, 255))
- 
-    
-    
+
     rainbow_colors(pixels)
-    
+
     brightness_decrease(pixels)
 
     rainbow_cycle_successive(pixels, wait=0.1)
