0307c6b1e09be169581931a6d57d4115ae90bda8
hdyson <hdyson@gmail.com>
2018-12-11 20:02:51 +0000

    Example commit to experiment with (#2)
    * Example commit to experiment with
    
    * Update tree.py

--- tree.py
+++ tree.py
@@ -1,22 +1,28 @@
+#!/usr/bin/env python
 # Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
 import time
 import pixel_strings_fncs as q
- 
+
 # Configure the count of pixels:
 PIXEL_COUNT = 46
 
-top = [x+37 for x in range(9)]
-middle = [x+22 for x in range(11)]
-bottom = range(18)
 
-pixels = q.initialise_pixels(PIXEL_COUNT)
+def set_tree():
+    '''Insert docstring here'''
+    top = range(37, 46)
+    middle = range(22, 33)
+    bottom = range(18)
 
-top_state    = q.make_rainbow_state(top)
-middle_state = q.make_rainbow_state(middle)
-bottom_state = q.make_rainbow_state(bottom)
-q.set_state(pixels, top, top_state)
-q.set_state(pixels, middle, middle_state)
-q.set_state(pixels, bottom, bottom_state)
-pixels.show()
+    pixels = q.initialise_pixels(PIXEL_COUNT)
+
+    top_state = q.make_rainbow_state(top)
+    middle_state = q.make_rainbow_state(middle)
+    bottom_state = q.make_rainbow_state(bottom)
+    q.set_state(pixels, top, top_state)
+    q.set_state(pixels, middle, middle_state)
+    q.set_state(pixels, bottom, bottom_state)
+    pixels.show()
 
 
+if __name__ == '__main__':
+    set_tree()
