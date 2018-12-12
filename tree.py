395453a0e4dd79d452f170d8ebe97abda542fa0a
pie-man <simon.the.pi.fan@gmail.com>
2018-12-12 12:56:42 +0000

    Add a function to coloour each of the rings red, green or blue

--- tree.py
+++ tree.py
@@ -2,6 +2,7 @@
 # Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
 import time
 import pixel_strings_fncs as q
+from random import randint
 
 # Configure the count of pixels:
 PIXEL_COUNT = 46
@@ -23,6 +24,18 @@ def set_tree():
     q.set_state(pixels, BOTTOM, bottom_state)
     pixels.show()
 
+def set_r_g_and_b_rings():
+    '''Set the three rings of the tree to display static
+       rings of red, green or blue'''
+    colours = [(200, 0, 0), (0, 200, 0), (0, 0, 200)]
+    top_state = q.make_colour_state(TOP, colours[randint(0,2)])
+    middle_state = q.make_colour_state(MIDDLE, colours[randint(0,2)])
+    bottom_state = q.make_colour_state(BOTTOM, colours[randint(0,2)])
+    q.set_state(pixels, TOP, top_state)
+    q.set_state(pixels, MIDDLE, middle_state)
+    q.set_state(pixels, BOTTOM, bottom_state)
+    pixels.show()
+
 def say_something():
     print("hello")
 
@@ -31,7 +44,8 @@ if __name__ == '__main__':
     set_tree()
     function_list = [
             set_tree,
-            say_something
+            say_something,
+            set_r_g_and_b_rings,
             ]
     for function in function_list:
         function()
