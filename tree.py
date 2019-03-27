6817e9b3ef155e6abd8dccfdf2132e6328b21ecb
pie-man <simon.the.pi.fan@gmail.com>
2019-03-27 23:26:29 +0000

    stuff me with a lemon....

--- tree.py
+++ tree.py
@@ -6,17 +6,21 @@ import pixel_strings_fncs as q
 from random import randint
 
 # Configure the count of pixels:
-PIXEL_COUNT = 46
+PIXEL_COUNT = 50
 
 # Define the 'index' of the three rings of pixels to be ued globally
-TOP    = range(37, 46)
-MIDDLE = range(22, 33)
-BOTTOM = range(18)
+TOP    = range(25, 50)
+MIDDLE = range(7, 24)
+BOTTOM = range(7)
+
+print_level=1
+threshold=1
 
 def set_rainbows():
     '''Set the three rings of the tree to display a static
        loop of rainbow colours'''
 
+    debug_print("enetered set_rainbows(): ")
     top_state = q.make_rainbow_state_rgb(TOP)
     middle_state = q.make_rainbow_state_rgb(MIDDLE)
     bottom_state = q.make_rainbow_state_rgb(BOTTOM)
@@ -29,6 +33,7 @@ def set_rainbows():
 def set_r_g_and_b_rings():
     '''Set the three rings of the tree to display static
        rings of red, green or blue'''
+    debug_print("enetered set_r_g_and_b_rings")
     colours = [(200, 0, 0), (0, 200, 0), (0, 0, 200)]
     top_state = q.make_colour_state(TOP, colours[randint(0,2)])
     middle_state = q.make_colour_state(MIDDLE, colours[randint(0,2)])
@@ -42,6 +47,7 @@ def set_r_g_and_b_rings():
 def set_random_rings():
     '''Set the three rings of the tree to display static
        rings of random colours'''
+    debug_print("enetered set_random_rings")
     top_state = q.make_colour_state(TOP, q.get_random_colour_rgb())
     middle_state = q.make_colour_state(MIDDLE, q.get_random_colour_rgb())
     bottom_state = q.make_colour_state(BOTTOM, q.get_random_colour_rgb())
@@ -53,6 +59,7 @@ def set_random_rings():
 
 def set_to_black():
     '''fade all three rings to black'''
+    debug_print("enetered set_to_black(): ")
     dummy = "not required"
     top_actor    = (q.fade_to_black, TOP, dummy, False)
     middle_actor = (q.fade_to_black, MIDDLE, dummy, False)
@@ -62,6 +69,7 @@ def set_to_black():
 
 def set_to_dim():
     '''fade all three rings to black'''
+    debug_print("enetered set_to_dim():")
     dummy = "not required"
     top_actor    = (q.fade_to_color_rgb, TOP, (1,1,1), False)
     middle_actor = (q.fade_to_color_rgb, MIDDLE, (1,1,1), False)
@@ -73,10 +81,45 @@ def cycling_rainbows():
     '''Set the three rings of the tree to display a rainbow
        pattern and gently cycle it around the ring.
        Please only run this AFTER set_to_rainbows'''
+    debug_print("enetered cycling_rainbows(): ")
     top_actor    = (q.rainbow_cycle_II, TOP   , (256, 256, 2), False)
     middle_actor = (q.rainbow_cycle_II, MIDDLE, (256, 256, 4), True)
     bottom_actor = (q.rainbow_cycle_II, BOTTOM, (256, 256, 6), False)
     return (top_actor, middle_actor, bottom_actor)
+
+def wibble_1(index):
+    debug_print("enetered wibble_1")
+    state = q.make_rainbow_state_rgb(index)
+    actor = (q.fade_to_state_rgb, index, state, False)
+    return actor
+
+def set_r_g_or_b_ring(index):
+    '''Set a ring to one of red, green or blue'''
+    debug_print("enetered set_r_g_or_b_ring")
+    colours = [(200, 0, 0), (0, 200, 0), (0, 0, 200)]
+    state = q.make_colour_state(index, colours[randint(0,2)])
+    actor = (q.fade_to_state_rgb, index, state, False)
+    return (actor)
+
+def set_right_random_wibblefest():
+    '''Selects 3 funtions randomly and assigns one to each ring'''
+    debug_print("enetered set_right_random_wibblefest(): ")
+    list_of_functions = [
+            wibble_1,
+            set_r_g_or_b_ring,
+            ]
+    selection = randint(0, len(list_of_functions)-1 )
+    top_actor    = list_of_functions[selection](TOP)
+    selection = randint(0, len(list_of_functions)-1 )
+    middle_actor = list_of_functions[selection](MIDDLE)
+    selection = randint(0, len(list_of_functions)-1 )
+    bottom_actor = list_of_functions[selection](BOTTOM)
+    return (top_actor, middle_actor, bottom_actor)
+
+def debug_print(text):
+    '''Check print level and print messages if threshold metdef '''
+    if print_level >= threshold:
+        print(text)
 
 if __name__ == '__main__':
     pixels = q.initialise_pixels(PIXEL_COUNT)
@@ -90,18 +133,24 @@ if __name__ == '__main__':
             set_to_dim,
             set_random_rings,
             set_to_dim,
+            set_right_random_wibblefest,
+            set_right_random_wibblefest,
+            set_right_random_wibblefest,
+            set_rainbows,
+            cycling_rainbows,
+            set_to_dim,
             ]
 
     count=len(function_list)-1
     #for function in function_list:
-    while datetime.now().hour < 18:
+    while datetime.now().hour > 18:
         count=count+1
         if count >= len(function_list):
             count=0
         function = function_list[count]
         (top_actor, middle_actor, bottom_actor) = function()
-        stepcount = 360
-        cluster = q.bundle(pixels, wait=0.5, steps=stepcount)
+        stepcount = 180
+        cluster = q.bundle(pixels, wait=0.2, steps=stepcount)
         cluster.add_function(top_actor[0], top_actor[1], attribute=top_actor[2],
                              reverse=top_actor[3])
         cluster.add_function(middle_actor[0], middle_actor[1], attribute=middle_actor[2],
