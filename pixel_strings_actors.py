7565d9693f17d11c141766eebc4c81d21da842d0
pie-man <sharp.roddy@gmail.com>
2024-09-23 15:53:30 +0100

    moving the subsplitting of lengths into a seperate function

--- pixel_strings_actors.py
+++ pixel_strings_actors.py
@@ -31,6 +31,55 @@ from pixel_strings_ext_libs import update_led_string
     lengths of the blocks, pluss possibly the fade times etc.
 '''
 
+def section_subsplitting(pixel_count, number_of_blocks, gap_ratio
+                         include_blank=False, max_blocks=False):
+    '''
+        Routine to break a length of pixels into subsections.
+        Takes the original length, the desired number of subsections and
+        the ratio of the 'illuminated' block to the gaps between blocks.
+        So a ration of 0.0 would have no gaps, 1.0 the gaps would be of equal
+        length to the iluminated blocks and 3.0 would make gaps 3 times longer
+        than illuminated blocks, meaning the illuminated section was 1/4 the
+        length of the sub-section.
+        The 'include_blank' boolian is for patterns where the run of
+        illuminated pixels is assumed to have a 'blank' one at the end so as it
+        travels along, no 'clean-up' is required to return to blank.
+        The 'max_blocks' boolian is for occasions where itn is desirable to
+        adjust the block/gap ration until it is as close as possible to the
+        ratio specified, but less than it making the length of illuminated
+        pixels the maximum.
+    '''
+    # Basic check - a -ve gap ratio makes no sense...
+    # Could raise an error - but actually want code to continue running as we're
+    # just illuminateing some lights here...
+    if gap_ratio < 0.0:
+        gap_ratio = 0.0
+    # First stab at getting the subsection length
+    section_length = int(pixel_count // number_of_blocks)
+    # More saftety checks. Initial issue was many blocks and a large gap ratio
+    # meant when converted to integers, there was no illuminated block
+    if section_length < 5:
+        number_of_blocks = int(pixel_count // 5)
+        print(f"resetting number of blocks to {number_of_blocks} due to short section length")
+        section_length = int(pixel_count // number_of_blocks)
+    # Actual source of error is when (1 + gap_ratio) > section_length means
+    # block_length is 0
+    block_length = int(section_length // (1 + gap_ratio))
+    gap_length = section_length - block_length
+#     print(f"section_length is {section_length}")
+#     print(f"block_length is {block_length}")
+#     print(f"gap_length is {gap_length}\n")
+    while (gap_length / block_length) > gap_ratio:
+#         print(f"Calculated {gap_length / block_length}, prepare for lift off...")
+        gap_length -= 1
+        block_length +=1
+#     print(f"Final calculated {gap_length / block_length}, We are in orbit")
+    #print(f"section_length is {section_length}")
+    #print(f"block_length is {block_length}")
+    #print(f"gap_length is {gap_length}\n")
+
+    return block_length
+
 def fade_to_state_HSV_a(count, old_state, new_state, steps=25):
     ''' A function to fade from one 'state' to another.
     This function assumes both 'states' are of length 'count' and that they are lists of tuples defining a colour in terms of Hue, Saturation and Value.
@@ -103,27 +152,30 @@ def zooming_blocks(pixel_count, old_state, new_state, steps=25, number_of_blocks
     print(f"In Zooming Blocks for {steps} steps")
     state = old_state
     #noblocks = 6
-    if gap_ratio < 0.0:
-        gap_ratio = 0.0
-    section_length = int(pixel_count // number_of_blocks)
-    if section_length < 5:
-        number_of_blocks = int(pixel_count // 5)
-        print(f"resetting number of blocks to {number_of_blocks} due to short section length")
-        section_length = int(pixel_count // number_of_blocks)
-    block_length = int(section_length // (1 + gap_ratio))
-    gap_length = section_length - block_length
-#     print(f"section_length is {section_length}")
-#     print(f"block_length is {block_length}")
-#     print(f"gap_length is {gap_length}\n")
-    while (gap_length / block_length) > gap_ratio:
-#         print(f"Calculated {gap_length / block_length}, prepare for lift off...")
-        gap_length -= 1
-        block_length +=1
-#     print(f"Final calculated {gap_length / block_length}, We are in orbit")
-    #print(f"section_length is {section_length}")
-    #print(f"block_length is {block_length}")
-    #print(f"gap_length is {gap_length}\n")
+##=-     if gap_ratio < 0.0:
+##=-         gap_ratio = 0.0
+##=-     section_length = int(pixel_count // number_of_blocks)
+##=-     if section_length < 5:
+##=-         number_of_blocks = int(pixel_count // 5)
+##=-         print(f"resetting number of blocks to {number_of_blocks} due to short section length")
+##=-         section_length = int(pixel_count // number_of_blocks)
+##=-     block_length = int(section_length // (1 + gap_ratio))
+##=-     gap_length = section_length - block_length
+##=- #     print(f"section_length is {section_length}")
+##=- #     print(f"block_length is {block_length}")
+##=- #     print(f"gap_length is {gap_length}\n")
+##=-     while (gap_length / block_length) > gap_ratio:
+##=- #         print(f"Calculated {gap_length / block_length}, prepare for lift off...")
+##=-         gap_length -= 1
+##=-         block_length +=1
+##=- #     print(f"Final calculated {gap_length / block_length}, We are in orbit")
+##=-     #print(f"section_length is {section_length}")
+##=-     #print(f"block_length is {block_length}")
+##=-     #print(f"gap_length is {gap_length}\n")
 
+    section_length, block_length, gap_length = section_subsplitting(
+                                        pixel_count, number_of_blocks, gap_ratio
+                                        include_blank=False, max_blocks=False):
     blocks=[]
     colours = []
     # Create a list of lists. Each lists contains a block of indecies for pixels
@@ -173,29 +225,32 @@ def bouncing_blocks(pixel_count, old_state, new_state, steps=25, number_of_block
     print(f"In Bouncing Blocks for {steps} steps")
     #noblocks = 6
     state = old_state
-    if gap_ratio < 0.0:
-        gap_ratio = 0.0
-    section_length = int(pixel_count // number_of_blocks)
-    if section_length < 5:
-        number_of_blocks = int(pixel_count // 5)
-        print(f"resetting number of blocks to {number_of_blocks} due to short section length")
-        section_length = int(pixel_count // number_of_blocks)
-    block_length = int(section_length // (1 + gap_ratio))
-    if block_length < 1:
-        block_length = 1
-    gap_length = section_length - block_length
-    #print(f"section_length is {section_length}")
-    #print(f"block_length is {block_length}")
-    #print(f"gap_length is {gap_length}\n")
-    while (gap_length / block_length) > gap_ratio:
-        #print(f"Calculated {gap_length / block_length}, prepare for lift off...")
-        gap_length -= 1
-        block_length +=1
-    #print(f"Final calculated {gap_length / block_length}, We are in orbit")
-    #print(f"section_length is {section_length}")
-    #print(f"block_length is {block_length}")
-    #print(f"gap_length is {gap_length}\n")
+##=-     if gap_ratio < 0.0:
+##=-         gap_ratio = 0.0
+##=-     section_length = int(pixel_count // number_of_blocks)
+##=-     if section_length < 5:
+##=-         number_of_blocks = int(pixel_count // 5)
+##=-         print(f"resetting number of blocks to {number_of_blocks} due to short section length")
+##=-         section_length = int(pixel_count // number_of_blocks)
+##=-     block_length = int(section_length // (1 + gap_ratio))
+##=-     if block_length < 1:
+##=-         block_length = 1
+##=-     gap_length = section_length - block_length
+##=-     #print(f"section_length is {section_length}")
+##=-     #print(f"block_length is {block_length}")
+##=-     #print(f"gap_length is {gap_length}\n")
+##=-     while (gap_length / block_length) > gap_ratio:
+##=-         #print(f"Calculated {gap_length / block_length}, prepare for lift off...")
+##=-         gap_length -= 1
+##=-         block_length +=1
+##=-     #print(f"Final calculated {gap_length / block_length}, We are in orbit")
+##=-     #print(f"section_length is {section_length}")
+##=-     #print(f"block_length is {block_length}")
+##=-     #print(f"gap_length is {gap_length}\n")
 
+    section_length, block_length, gap_length = section_subsplitting(
+                                        pixel_count, number_of_blocks, gap_ratio
+                                        include_blank=False, max_blocks=False):
     blocks=[]
     colours = []
     # Create a list of lists. Each lists contains a block of indecies for pixels
---
+++ temp.py
@@ -0,0 +1,48 @@
+def section_subsplitting(pixel_count, number_of_blocks, gap_ratio
+                         include_blank=False, max_blocks=False):
+    '''
+        Routine to break a length of pixels into subsections.
+        Takes the original length, the desired number of subsections and
+        the ratio of the 'illuminated' block to the gaps between blocks.
+        So a ration of 0.0 would have no gaps, 1.0 the gaps would be of equal
+        length to the iluminated blocks and 3.0 would make gaps 3 times longer
+        than illuminated blocks, meaning the illuminated section was 1/4 the
+        length of the sub-section.
+        The 'include_blank' boolian is for patterns where the run of
+        illuminated pixels is assumed to have a 'blank' one at the end so as it
+        travels along, no 'clean-up' is required to return to blank.
+        The 'max_blocks' boolian is for occasions where itn is desirable to
+        adjust the block/gap ration until it is as close as possible to the
+        ratio specified, but less than it making the length of illuminated
+        pixels the maximum.
+    '''
+    # Basic check - a -ve gap ratio makes no sense...
+    # Could raise an error - but actually want code to continue running as we're
+    # just illuminateing some lights here...
+    if gap_ratio < 0.0:
+        gap_ratio = 0.0
+    # First stab at getting the subsection length
+    section_length = int(pixel_count // number_of_blocks)
+    # More saftety checks. Initial issue was many blocks and a large gap ratio
+    # meant when converted to integers, there was no illuminated block
+    if section_length < 5:
+        number_of_blocks = int(pixel_count // 5)
+        print(f"resetting number of blocks to {number_of_blocks} due to short section length")
+        section_length = int(pixel_count // number_of_blocks)
+    # Actual source of error is when (1 + gap_ratio) > section_length means
+    # block_length is 0
+    block_length = int(section_length // (1 + gap_ratio))
+    gap_length = section_length - block_length
+#     print(f"section_length is {section_length}")
+#     print(f"block_length is {block_length}")
+#     print(f"gap_length is {gap_length}\n")
+    while (gap_length / block_length) > gap_ratio:
+#         print(f"Calculated {gap_length / block_length}, prepare for lift off...")
+        gap_length -= 1
+        block_length +=1
+#     print(f"Final calculated {gap_length / block_length}, We are in orbit")
+    #print(f"section_length is {section_length}")
+    #print(f"block_length is {block_length}")
+    #print(f"gap_length is {gap_length}\n")
+
+    return block_length
